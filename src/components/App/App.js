5eceeb6a885695f20c33a63dc9c44f03dfb89fff
Furqan25 <66711831+Furqan25@users.noreply.github.com>
2022-06-08 14:03:20 -0400

    first react code

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from '../Header/Header';
+
+export default function App() {
+    const name = 'Company Name';
+    return (
+        <div className='App'>
+            <Header company={name} title="some stuff"/>
+        </div>
+    );
+}
+
+
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import './nav.css'
+
+export default function Nav(props){
+    return (
+        <nav>
+            Links here
+            <h1>{props.ttl}</h1>
+        </nav>
+        
+    )
+}
